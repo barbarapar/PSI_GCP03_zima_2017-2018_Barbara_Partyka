2c614b16d470be281136155563daa15a281fd4c0
barbarapar <34322379+barbarapar@users.noreply.github.com>
2018-01-19 09:53:36 +0100

    Add files via upload

---
+++ PSI-Scenariusz-06/PSI-06/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String args[]){
+
+        Network network = new Network(0.95, 40, 30, 10000);
+        network.learning();
+        network.testing();
+
+
+
+    }
+}
